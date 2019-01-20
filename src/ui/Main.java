df831142c53ae586941b0a71cc0e144e8d54b7de
Malin Albinsson <mea733@telia.com>
2019-01-20 01:23:53 +0100

    Added class Main

---
+++ src/ui/Main.java
@@ -0,0 +1,19 @@
+package ui;
+
+/**
+ * The Class Main.
+ */
+public class Main {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	// main method
+		public static void main(String[] args) {
+			UI ui = new UI();
+			ui.runUI();
+			
+		}
+}
